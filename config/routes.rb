78acdef37ee0beb484ab830ef5f4328ebf5ce4b9
yutomizuta <yuto11752@i.softbank.jp>
2022-11-12 20:15:16 +0900

    [add]homesのcontroller,view,routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
   # 学生用
-  devise_for :customers,skip: [:passwords], controllers: {
+  devise_for :students,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
@@ -9,4 +13,9 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
+
+  scope module: :public do
+    root to: 'homes#top'
+    get "/about" => "homes#about"
+  end
 end
