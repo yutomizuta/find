058170764348152eeb245e8fe5e341be01cfa9a6
yutomizuta <yuto11752@i.softbank.jp>
2022-11-12 19:20:41 +0900

    [add]deviseのコントローラとview

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :students
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # 学生用
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  # 管理者用
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
 end
