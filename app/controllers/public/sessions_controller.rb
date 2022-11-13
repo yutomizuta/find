aed8bd68989158f5e427b6f5190a841f8bed42c0
yutomizuta <yuto11752@i.softbank.jp>
2022-11-14 02:19:09 +0900

    signup,sign後に自分の詳細へ遷移

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -24,4 +24,7 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  def after_sign_in_path_for(resource)
+    student_path(resource)
+  end
 end
