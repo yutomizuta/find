f919fbb0b34c0984ad4ece1337195a3a37899714
yutomizuta <yuto11752@i.softbank.jp>
2022-11-17 20:52:41 +0900

    [add]時間割table

--- app/models/taken_course.rb
+++ app/models/taken_course.rb
@@ -7,4 +7,10 @@ class TakenCourse < ApplicationRecord
   enum period:      { １限: 0, ２限: 1, ３限: 2, ４限: 3, ５限: 4, 指定なし: 5 }
 
   belongs_to :student
+
+  # 曜日と時間で時間割に埋め込み
+  def self.schedule(student, day_of_week, priod)
+    find_by(student_id: student, day_of_week: day_of_week, period: priod)&.name
+  end
+  
 end
