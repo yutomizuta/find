53abb31b05d04a48ae909d3d7449ded481c96238
yutomizuta <yuto11752@i.softbank.jp>
2022-11-18 18:39:28 +0900

    [add]コメントアウト

--- app/models/student.rb
+++ app/models/student.rb
@@ -6,7 +6,9 @@ class Student < ApplicationRecord
 
   has_one_attached :profile_image
 
+  # 画像の設定
   def get_profile_image(width, height)
+    # 画像未設定の場合no_image.jpgを設定する
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
@@ -43,6 +45,7 @@ class Student < ApplicationRecord
   has_many :student_rooms, dependent: :destroy
   has_many :chats, dependent: :destroy
 
+  # フォロー機能
   def follow(student)
     relationships.create(followed_id: student.id)
   end
